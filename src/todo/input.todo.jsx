20eecc709844fafff289b40c4e8f8be82c7a833c
minh <miyamuradesuu@gmail.com>
2026-01-28 15:16:12 +0700

    definition of State and useState Hook

--- src/todo/input.todo.jsx
+++ src/todo/input.todo.jsx
@@ -1,13 +1,30 @@
+import { useState } from "react"; // use => hook
+
+let count = 1;
+
 const InputTodo = () => {
 
     const title = "Add new todo";
 
+    // state có 2 thành phần -> tên và function cập nhật giá trị biến
+    const [fullName, setFullName] = useState("")
+
+    const handleClick = () => {
+        alert("Click me")
+    }
+
     return (
         <div>
+            <div> new count = {count}</div>
             <h1>{title}</h1>
-            <input type="text" />
+            <input type="text"
+                onChange={(event) => {
+                    setFullName(event.target.value)
+                }}
+            />
             &nbsp; &nbsp;
-            <button>Save</button>
+            <div>My todo is {fullName}</div>
+            <button onClick={() => handleClick()}>Save</button>
         </div>
     )
 }
