ed0f89bdb5a3a7b241e03945d91ac635c6c3c73c
minh <miyamuradesuu@gmail.com>
2026-01-28 14:53:23 +0700

    Initial commit

---
+++ src/todo/input.todo.jsx
@@ -0,0 +1,14 @@
+const InputTodo = () => {
+
+    const title = "Add new todo";
+
+    return (
+        <div>
+            <h1>{title}</h1>
+            <input type="text" />
+            &nbsp; &nbsp;
+            <button>Save</button>
+        </div>
+    )
+}
+export default InputTodo;
